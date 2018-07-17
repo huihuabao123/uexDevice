3177bf9092a0959163f3803dd6b4f60be00f271a
pengliang <pengliang@huihuabao.com>
2018-07-17 14:34:16 +0800

    平板底部导航栏

--- src/org/zywx/wbpalmstar/plugin/uexdevice/EUExDevice.java
+++ src/org/zywx/wbpalmstar/plugin/uexdevice/EUExDevice.java
@@ -1065,11 +1065,23 @@ public class EUExDevice extends EUExBase {
             mContext.unregisterReceiver(mConnectChangeReceiver);
     }
 
+//    public void hideStatusBar(String[] params) {
+//        View decorView = ((Activity) mContext).getWindow().getDecorView();
+//        int systemUiVisibility = decorView.getSystemUiVisibility();
+//        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+//        systemUiVisibility |= flags;
+//        decorView.setSystemUiVisibility(systemUiVisibility);
+//    }
     public void hideStatusBar(String[] params) {
         View decorView = ((Activity) mContext).getWindow().getDecorView();
         int systemUiVisibility = decorView.getSystemUiVisibility();
         int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
